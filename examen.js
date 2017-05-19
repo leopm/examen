9e0557589256364fd9cc7d8fdebfc7ac8d50e25f
Leandro Peñafiel <leopm87@gmail.com>
2017-05-19 15:51:31 -0500

    correccion de formulas

--- examen.js
+++ examen.js
@@ -11,24 +11,30 @@ var resultado;
 
 if(listaCapacidad==1){
   capacidad= capacidadFormulario;
+  espacio= (espacioLibreFormulario*1024)/100;
 }if(listaCapacidad==2){
   capacidad= capacidadFormulario*1024;
+  espacio=(capacidad*espacioLibreFormulario)/100;
 }if(listaCapacidad==3){
-  capacidad= (((capacidadFormulario*1024)/1)*1024)/1;
+  capacidad= (capacidadFormulario*1024)*1024;
+  espacio=(capacidad*espacioLibreFormulario)/100;
 }
 
-espacio= (capacidad*espacioLibreFormulario)/100;
+
 
 if(listaArchivo==1){
   archivo= archivoFormulario;
 }if(listaArchivo==2){
   archivo= archivoFormulario*1024;
 }if(listaArchivo==3){
-  archivo= (((archivoFormulario*1024)/1)*1024)/1;
+  archivo= (archivoFormulario*1024)*1024;
 }
 
-resultado=(capacidad-espacio)-archivo;
-(resultado<0)? alert("no hay espacio"):alert("si hay espacio "+resultado);
+
+
+resultado=espacio-archivo;
+
+(resultado<0)? alert("no hay espacio en la memoria usb"):alert("si hay espacio en la memoria usb");
 
 
 
